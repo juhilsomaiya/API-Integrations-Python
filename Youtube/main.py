4723702ae4fea32b255939e197ad3b145cd397f5
Juhil Somaiya <jgs.131296@gmail.com>
2018-09-14 14:26:45 +0530

    Create main.py

---
+++ Youtube/main.py
@@ -0,0 +1,44 @@
+import argparse
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+DEVELOPER_KEY = 'Replace with yours'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+
+def youtube_search(options):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
+    search_response = youtube.search().list(q=options.q, part='id,snippet', maxResults=options.max_results).execute()
+
+    videos = []
+    channels = []
+    playlists = []
+
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            videos.append('%s' % (search_result['snippet']['title']))
+        elif search_result['id']['kind'] == 'youtube#channel':
+            channels.append('%s' % (search_result['snippet']['title']))
+        elif search_result['id']['kind'] == 'youtube#playlist':
+            playlists.append('%s' % (search_result['snippet']['title']))
+
+    print("{:>30}".format("Videos"), '\n',  "--"*45)
+    print('\n'.join(videos), '\n')
+    print("{:>30}".format("Channels"), '\n',  "--"*45)
+    print ('\n'.join(channels), '\n')
+    print("{:>30}".format("Playlists"), '\n',  "--"*45)
+    print ('\n'.join(playlists), '\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--q', help='Search term', default='Flutter')
+    parser.add_argument('--max-results', help='Max results', default=35)
+    args = parser.parse_args()
+
+try:
+    youtube_search(args)
+except HttpError:
+    print ("Error")
